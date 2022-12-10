6e2be976ecf1cc3f91fe8e1c02e779e59b4e4cc3
RBBRod <RubberRod.Prod@gmail.com>
2022-12-10 18:44:23 +0100

    Improved App Graphics

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,9 +16,9 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (32.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.setFont (juce::Font (40.f));
+    g.setColour (juce::Colours::red);
+    g.drawText ("Hello I'm a new PFM::C++ Student", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
